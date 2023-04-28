35a99e6bda3afe3d79c77120bd29a31e1fee07e9
juanyepesp <j.yepes@uniandes.edu.co>
2023-04-27 21:12:09 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the review and model from the form data
+    review = request.form['review']
+    model = request.form['model']
+
+    # Define the API endpoint for the selected model
+    endpoint = f'http://localhost:8000/predict/{model}'
+
+    # Send the review data to the API endpoint
+    response = requests.post(endpoint, json={'review_es': review})
+
+    # Parse the prediction from the API response
+    prediction = response.json()
+
+    if prediction == 'positive':
+        prediction = 'Positivo'
+    else:
+        prediction = 'Negativo'
+
+    # Render the HTML template with the prediction
+    return render_template('index.html', prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
